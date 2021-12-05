7df9c48e2c47b0ea6a6e4d2dfbd435006e4673fb
buihuuchau <buihuuchau99@gmail.com>
2021-12-05 09:32:46 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,25 @@
-import "./App.css";
-import { Button } from "react-bootstrap";
-import Header from "./Header";
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import AddProduct from "./AddProduct";
-import UpdateProduct from "./Update";
-import Protected from "./Protected";
-import ListProduct from "./ListProduct";
-function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/register">
-                        <Register />
-                    </Route>
+import logo from './logo.svg';
+import './App.css';
 
-                    <Route path="/add">
-                        <Protected Cmp={AddProduct} />
-                        {/* <AddProduct /> */}
-                    </Route>
-                    <Route path="/update/:id">
-                        <Protected Cmp={UpdateProduct} />
-                        {/* <UpdateProduct /> */}
-                    </Route>
-                    <Route path="/">
-                        <Protected Cmp={ListProduct} />
-                        {/* <ListProduct /> */}
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
